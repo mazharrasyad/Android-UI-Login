b08d99d98313fb0d632052e1f638082131300041
mazharrasyad <muhazharrasyad@gmail.com>
2020-12-05 09:26:13 +0700

    passing data, profil, logout

--- app/src/main/java/com/example/tampilanlogin/LoginActivity.java
+++ app/src/main/java/com/example/tampilanlogin/LoginActivity.java
@@ -6,23 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnLogin;
+    private Button button_login;
+    private EditText edit_username, edit_password;
+    private String text_username, text_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnLogin = findViewById(R.id.btn_login);
+        initView();
+        setData();
+    }
 
-        btnLogin.setOnClickListener(new View.OnClickListener(){
+    public void initView(){
+        button_login = findViewById(R.id.button_login_id);
+        edit_username = findViewById(R.id.edit_username_id);
+        edit_password = findViewById(R.id.edit_password_id);
+    }
+
+    public void setData(){
+        button_login.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent pindah = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(pindah);
+                text_username = edit_username.getText().toString();
+                text_password = edit_password.getText().toString();
+
+                Intent login = new Intent(LoginActivity.this, MainActivity.class);
+                login.putExtra("username", text_username);
+                login.putExtra("password", text_password);
+                startActivity(login);
             }
         });
     }
--- app/src/main/java/com/example/tampilanlogin/MainActivity.java
+++ app/src/main/java/com/example/tampilanlogin/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.tampilanlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text_username, text_password;
+    private String username, password;
+    private Button button_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        setData();
+        logout();
+    }
+
+    public void initView(){
+        text_username = findViewById(R.id.text_username_id);
+        text_password = findViewById(R.id.text_password_id);
+        button_logout = findViewById(R.id.button_logout_id);
+    }
+
+    public void setData(){
+        Intent login = getIntent();
+        username = login.getStringExtra("username");
+        password = login.getStringExtra("password");
+
+        text_username.setText(username);
+        text_password.setText(password);
+    }
+
+    public void logout(){
+        button_logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent logout = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(logout);
+            }
+        });
     }
 }
